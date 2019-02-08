02854eb18d6a2897e9441a19614af15ec5100a90
BuildTools <unconfigured@null.spigotmc.org>
2019-02-08 09:40:50 +0100

    retrieve information from JIRA rest API and create an array of objects

--- getJiraIssuesFromProject/index.js
+++ getJiraIssuesFromProject/index.js
@@ -1,5 +1,39 @@
-console.log("before function") 
+const request = require('request');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var btoa = require('btoa');
+
+issuesResponse = []
+
 exports.handler = (event, context, callback) => {
-  console.log("inside function") 
-  callback(null, "NO");
+
+  url = "http://devjira.oskar-ruegg.com/rest/api/2/search?jql=project=HELLA&fields=summary,fixVersions"
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+            // console.log(xhttp.responseText)
+            userJson = JSON.parse(xhttp.responseText)
+            if(userJson.issues.length>0) {
+              issues = userJson.issues
+              issues.forEach(function (element) {
+		issue = {}
+                issue.key = element.key
+                if(element.fields.fixVersions[0])
+                  issue.fixVersion = element.fields.fixVersions[0].name
+                issuesResponse.push(issue) 
+              })
+            }
+            setTimeout(function() { callback(null, "NO Groups Found"); }, 500);
+       }
+       // console.log(issuesResponse)
+       // callback.done(null, JSON.stringify(issuesResponse))
+       //callback(null, JSON.stringify(issuesResponse))
+       xactivate.send(JSON.stringify(issuesResponse))
+    };
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader( 'Authorization', 'Basic ' + btoa( "techuser" + ':' + "techuser" ) )
+    xhttp.send();
+
+
 }
